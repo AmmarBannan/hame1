5f024e9a125722055c2016fe0f96782a08861d5c
ammarbannan <ammarabannan@gmail.com>
2025-07-09 05:44:52 +0300

    copied

--- game.gd
+++ game.gd
@@ -1,48 +1,48 @@
-extends Node
 class_name Game
+extends Node
 
-const PLAYER =preload("res://player/player.tscn")
-var peer=ENetMultiplayerPeer.new()
-var players:Array[Player]=[]
-@onready var multiplayer_ui=$ui/multiplayer
-@onready var oid_lbl=$ui/multiplayer/VBoxContainer/OID
-@onready var oid_input=$ui/multiplayer/VBoxContainer/OIDInput
+@onready var multiplayer_ui = $ui/multiplayer
+@onready var oid_lbl = $ui/multiplayer/VBoxContainer/OID
+@onready var oid_input = $ui/multiplayer/VBoxContainer/OIDInput
 
+const PLAYER = preload("res://player/player.tscn")
 
-func _ready() -> void:
-	$MultiplayerSpawner.spawn_function=addPlayer
+var peer = ENetMultiplayerPeer.new()
+var players: Array[Player] = []
+
+func _ready():
+	$MultiplayerSpawner.spawn_function = add_player
+	
 	await Multiplayer.noray_connected
-	oid_lbl.text=Noray.oid
-func _on_host_pressed() -> void:
+	oid_lbl.text = Noray.oid
+
+func _on_host_pressed():
 	Multiplayer.host()
-	#peer.create_server(25565)
-	#multiplayer.multiplayer_peer=peer
+	
 	multiplayer.peer_connected.connect(
 		func(pid):
-			print("Player ID: ",pid)
+			print("Peer " + str(pid) + " has joined the game!")
 			$MultiplayerSpawner.spawn(pid)
 	)
+	
 	$MultiplayerSpawner.spawn(multiplayer.get_unique_id())
-	$ui/multiplayer.hide()
-	
-func _on_join_pressed() -> void:
+	multiplayer_ui.hide()
+
+func _on_join_pressed():
 	Multiplayer.join(oid_input.text)
-	#peer.create_client("localhost",25565)
-	#multiplayer.multiplayer_peer=peer
-	$ui/multiplayer.hide()
-
-func addPlayer(pid):
-	var player=PLAYER.instantiate()
-	player.name=str(pid)
-	player.global_position=$level.get_child(players.size()).global_position
-	players.append(player)
-	add_child(player)
-
-func get_random_spawn_Point():
-	return $level.get_children().pick_random().global_position
 	
+	multiplayer_ui.hide()
 
+func add_player(pid):
+	var player = PLAYER.instantiate()
+	player.name = str(pid)
+	player.global_position = $level.get_child(players.size()).global_position
+	players.append(player)
+	
+	return player
 
+func get_random_spawnpoint():
+	return $Level.get_children().pick_random().global_position
 
-func _on_copy_oid_pressed() -> void:
-	DisplayServer.clipboard_get()
+func _on_copy_oid_pressed():
+	DisplayServer.clipboard_set(Noray.oid)
